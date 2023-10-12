20dd37b8f69b27825def8bddb3e9246cd94e49f2
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-12 19:58:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["c", "c", "c", "c"],
+  ["c", "c", "c", "d"]];
 
 const whereIsWaldo = (matrix) => {
-  
-}
+  const initial = matrix[0][0]
+  for(let i=0; i< matrix.length; i++){
+       for(let j=0; j < matrix[i].length; j++){
+           if(matrix[i][j] !== initial) return [i+1,j+1]   
+        }
+    }
+  }
+
+
+console.log(`Found Waldo in: `, whereIsWaldo(example))
+/*
+
+ 
+*/
+
+/*
+return the corrdinate [row, column] for the different char in a matrix 
+full of same char, no matter the size of the matrix is.
+*/
