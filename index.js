073d60d87bf68789ff62eaefad29e722e51b716c
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-14 19:11:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const example = [
-  ["c", "c", "c", "c"],
-  ["c", "c", "c", "d"]];
+  ["O", "O", "O", "P"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"]
+];
+
 
 const whereIsWaldo = (matrix) => {
-  const initial = matrix[0][0]
-  for(let i=0; i< matrix.length; i++){
-       for(let j=0; j < matrix[i].length; j++){
-           if(matrix[i][j] !== initial) return [i+1,j+1]   
-        }
+  const initial = matrix[0][0];
+  if(initial !== matrix[0][1] && matrix[0][1] === matrix[0][2]) return [1, 1]
+
+  else{
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if(initial !== matrix[i][j]) return [i+1, j+1]
     }
   }
+}
+};
 
 
 console.log(`Found Waldo in: `, whereIsWaldo(example))
